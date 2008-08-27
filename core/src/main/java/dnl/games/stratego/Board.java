59de44387b56c9b489b0a2c4261ad97e84825af6
daniel.or <daniel.or@67dde4b7-e954-0410-9259-f34e3d579966>
2008-08-27 07:36:16 +0000

    refacts

--- core/src/main/java/dnl/games/stratego/Board.java
+++ core/src/main/java/dnl/games/stratego/Board.java
@@ -1,4 +1,4 @@
-package dnl.games.stratego.server;
+package dnl.games.stratego;
 
 import java.util.HashMap;
 import java.util.Map;
--- ui/src/main/java/dnl/games/stragego/ui/BoardLayer2.java
+++ ui/src/main/java/dnl/games/stragego/ui/BoardLayer2.java
@@ -1,5 +1,6 @@
 package dnl.games.stragego.ui;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -21,7 +22,20 @@ class BoardLayer2 extends JPanel {
 	public void paintComponent(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
 		g2.drawImage(image, 0, 0, this);
-		System.err.println("-----");
+		double w = getWidth();
+		double h = getHeight();
+		
+		g2.setColor(Color.red.darker());
+		
+		for (int i = 0; i < 10; i++) {
+			int y = (int)((h/10)*i);
+			g2.drawLine(0, y, (int)w, y);
+		}
+
+		for (int i = 0; i < 10; i++) {
+			int x = (int)((w/10)*i);
+			g2.drawLine(x, 0, x, (int)h);
+		}
 	}
 
 	/**
