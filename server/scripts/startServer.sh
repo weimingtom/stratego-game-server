cec2da2188cebac7f6f4dfa7b8ca3f657191b4d0
daniel.or <daniel.or@67dde4b7-e954-0410-9259-f34e3d579966>
2008-09-15 08:34:48 +0000

    well

--- server/scripts/startServer.sh
+++ server/scripts/startServer.sh
@@ -1,2 +1,8 @@
 mkdir -p ../target/data/Stratego/dsdb
-/home/daniel/Aps/Games/DarkStar/sgs-0.9.6-r4193/sgs.sh ../target/classes SgsStratego.properties
+export MVN_REPO=/home/daniel/Tools/mvn-repo
+export JAVA_OPTS="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8000"
+export CLASSPATH=../target/classes
+export CLASSPATH=${CLASSPATH}:${MVN_REPO}/dnl/games/stratego-game-core/1.0/stratego-game-core-1.0.jar
+export CLASSPATH=${CLASSPATH}:${MVN_REPO}/commons-lang/commons-lang/2.3/commons-lang-2.3.jar
+
+/home/daniel/Aps/Games/DarkStar/sgs-0.9.6-r4193/sgs.sh ${CLASSPATH} ./SgsStratego.properties
